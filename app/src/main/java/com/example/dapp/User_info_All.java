7f8d9be489040eaf920f9768bbbe93f02c688681
Nathan <18201104668@163.com>
2018-01-26 22:22:37 +0800

    编辑界面（明天做功能吧。。累）

--- app/src/main/java/com/example/dapp/User_info_All.java
+++ app/src/main/java/com/example/dapp/User_info_All.java
@@ -3,7 +3,8 @@ package com.example.dapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 
 public class User_info_All extends AppCompatActivity {
@@ -11,7 +12,14 @@ public class User_info_All extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_info_edit);
-        ListView ls = findViewById(R.id.user_info_lv);
-
+        Toolbar toolbar = findViewById(R.id.user_info_edit_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.getBackground().setAlpha(0);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User_info_All.this.finish();
+            }
+        });
     }
 }
