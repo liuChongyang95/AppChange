83b6dbcc71ba75115009f929121096be45a9a114
Nathan <18201104668@163.com>
2018-04-02 23:04:58 +0800

    自登陆功能，待修改

---
+++ app/src/main/java/Util/EnergyUnit.java
@@ -0,0 +1,9 @@
+package Util;
+
+public class EnergyUnit {
+    public int unit(int change){
+        int unit=1;
+        return unit;
+    }
+
+}
--- app/src/main/java/com/example/dapp/Login.java
+++ app/src/main/java/com/example/dapp/Login.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -35,9 +36,10 @@ public class Login extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private CheckBox rememberPass;
     private String intent_Userid;
+    private Boolean flag;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= 21) {
             View view = getWindow().getDecorView();
@@ -47,71 +49,81 @@ public class Login extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
-        setContentView(R.layout.login_app);
-        userDao = new UserDao(Login.this);
         //保存基本信息，SharedPreferences键值方式保存信息
         pref = PreferenceManager.getDefaultSharedPreferences(this);
-        rememberPass = findViewById(R.id.remember_pass);
         //取之前的值，是否勾选
         boolean isRemember = pref.getBoolean("remember_password", false);
-        username = findViewById(R.id.user_name);
-        password = findViewById(R.id.user_password);
-        register = findViewById(R.id.register_button);
-        login = findViewById(R.id.login_button);
-        register.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
-        register.setTextColor(Color.RED);
-        register.setClickable(true);
-        //判断之前是否勾选，没有则置空
+        userDao = new UserDao(Login.this);
         if (isRemember) {
             username_str = pref.getString("username_pref", "");
             password_str = pref.getString("password_pref", "");
-            username.setText(username_str);
-            password.setText(password_str);
-            rememberPass.setChecked(true);
-        }
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //记住密码
-                username_str = username.getText().toString().trim();
-                password_str = password.getText().toString().trim();
-                Boolean flag = userDao.login(username_str, password_str);
-                if (flag) {
-                    //如果勾选
-                    editor = pref.edit();
-                    if (rememberPass.isChecked()) {
-                        editor.putString("username_pref", username_str);
-                        editor.putString("password_pref", password_str);
-                        editor.putBoolean("remember_password", true);
-                    } else {
-                        editor.clear();
-                    }
-                    editor.apply();
-                    Toast.makeText(Login.this, "登录成功", Toast.LENGTH_SHORT).show();
+            flag = userDao.login(username_str, password_str);
+            setContentView(R.layout.skiplogin);
+
+            intent_Userid = userDao.getUserId(username_str);
+            if (flag) {
+                Intent intent = new Intent();
+                intent.setClass(Login.this, AllFunction.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("from_Login_User_id", intent_Userid);
+                bundle.putString("from_Login_User_Username", username_str);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }else {
+                Toast.makeText(this, "密码已被更改", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            setContentView(R.layout.login_app);
+            rememberPass = findViewById(R.id.remember_pass);
+            username = findViewById(R.id.user_name);
+            password = findViewById(R.id.user_password);
+            register = findViewById(R.id.register_button);
+            login = findViewById(R.id.login_button);
+            register.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+            register.setTextColor(Color.RED);
+            register.setClickable(true);
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //记住密码
                     username_str = username.getText().toString().trim();
                     password_str = password.getText().toString().trim();
-                    intent_Userid = userDao.getUserId(username_str);
-                    Intent intent = new Intent();
-                    intent.setClass(Login.this, AllFunction.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("from_Login_User_id", intent_Userid);
-                    bundle.putString("from_Login_User_Username", username_str);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                } else {
-                    //弹出正确错误信息的方式
-                    String flag_cause = userDao.failedCause(username_str, password_str);
-                    Toast.makeText(Login.this, flag_cause, Toast.LENGTH_SHORT).show();
+                    flag = userDao.login(username_str, password_str);
+                    if (flag) {
+                        //如果勾选
+                        editor = pref.edit();
+                        if (rememberPass.isChecked()) {
+                            editor.putString("username_pref", username_str);
+                            editor.putString("password_pref", password_str);
+                            editor.putBoolean("remember_password", true);
+                        } else {
+                            editor.clear();
+                        }
+                        editor.apply();
+                        Toast.makeText(Login.this, "登录成功", Toast.LENGTH_SHORT).show();
+                        intent_Userid = userDao.getUserId(username_str);
+                        Intent intent = new Intent();
+                        intent.setClass(Login.this, AllFunction.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("from_Login_User_id", intent_Userid);
+                        bundle.putString("from_Login_User_Username", username_str);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    } else {
+                        //弹出正确错误信息的方式
+                        String flag_cause = userDao.failedCause(username_str, password_str);
+                        Toast.makeText(Login.this, flag_cause, Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
-        //跳转到注册界面
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Register.class);
-                startActivity(intent);
-            }
-        });
+            });
+            //跳转到注册界面
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Login.this, Register.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
