27b503b1e8856fa7866a14a5b290d6ed8b59e03b
Nathan <18201104668@163.com>
2018-05-29 23:25:49 +0800

    没有后台，暂时不做

--- app/src/main/java/com/example/dapp/DietaryStatus.java
+++ app/src/main/java/com/example/dapp/DietaryStatus.java
@@ -2,11 +2,17 @@ package com.example.dapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,6 +20,14 @@ import android.view.View;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXImageObject;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXTextObject;
+import com.tencent.mm.opensdk.modelmsg.WXWebpageObject;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +51,8 @@ public class DietaryStatus extends AppCompatActivity {
     private static final String TAG = "DietaryStatus";
     private String foodName;
     private WebView webView;
+    private static final String APP_ID = "wxafc530d47fd59207";
+    private IWXAPI iwxapi;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -51,6 +67,8 @@ public class DietaryStatus extends AppCompatActivity {
             this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.dietary_doughnut);
+//        页面微信绑定
+        reg2WX();
         Toolbar toolbar = findViewById(R.id.dietaryToolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -102,17 +120,64 @@ public class DietaryStatus extends AppCompatActivity {
         }
         dataJson2JS = gson.toJson(stringIntegerHashMap);
         webView = findViewById(R.id.dietrayDoughnut);
-        WebSettings webSettings=webView.getSettings();
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setUseWideViewPort(true);
         webSettings.getBuiltInZoomControls();
         webSettings.setDefaultTextEncodingName("UTF-8");
         webView.loadUrl("file:///android_asset/web/Doughnut.html");
-        webView.setWebViewClient(new WebViewClient(){
+        webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                view.loadUrl("javascript: endEXE('"+dataJson2JS+"')");
+                view.loadUrl("javascript: endEXE('" + dataJson2JS + "')");
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.wxshare, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.wxShare:
+                Log.d(TAG, "onOptionsItemSelected: ");
+                shared2WX();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void reg2WX() {
+        iwxapi = WXAPIFactory.createWXAPI(DietaryStatus.this, APP_ID, true);
+        iwxapi.registerApp(APP_ID);
+    }
+
+    public void shared2WX() {
+//        ①包裹要发送的信息类型
+        WXWebpageObject webpageObject=new WXWebpageObject();
+        webpageObject.webpageUrl="www.me11571467-1.icoc.me";
+//         包裹要发送的信息
+        WXMediaMessage wxMediaMessage = new WXMediaMessage();
+        wxMediaMessage.mediaObject =webpageObject; //msg.mediaObject实际上是个IMediaObject对象, 比如WXTextObject对应发送的信息是文字,想要发送文字直接传入WXTextObject对象就行
+        wxMediaMessage.description="当日饮食情况(测试)";
+        wxMediaMessage.title="糖Dapp";
+//        ②创建缩略图
+        BitmapDrawable wxShare = (BitmapDrawable) getResources().getDrawable(R.drawable.wxshare);
+        Bitmap wxSharebm=Bitmap.createScaledBitmap(wxShare.getBitmap(),120,120,true);
+        wxMediaMessage.setThumbImage(wxSharebm);
+        wxSharebm.recycle();
+//        ③请求对象
+        //创建请求对象
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        //唯一的请求字段
+        req.transaction = String.valueOf(System.currentTimeMillis());
+        req.message = wxMediaMessage;
+//        发送到朋友圈  req.scene 默认发送个人WXSceneSession
+//        req.scene = SendMessageToWX.Req.WXSceneTimeline;
+        iwxapi.sendReq(req);
     }
 }
---
+++ app/src/main/java/com/example/dapp/WXEntryActivity.java
@@ -0,0 +1,41 @@
+package com.example.dapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelmsg.WXTextObject;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+
+
+public class WXEntryActivity extends AppCompatActivity implements IWXAPIEventHandler {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+        switch (baseResp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                Toast.makeText(this, "分享成功.", Toast.LENGTH_SHORT).show();
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                Toast.makeText(this, "取消分享.", Toast.LENGTH_SHORT).show();
+                break;
+            case BaseResp.ErrCode.ERR_SENT_FAILED:
+                Toast.makeText(this, "分享失败.", Toast.LENGTH_SHORT).show();
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                Toast.makeText(this, "分享被拒绝.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
